deeb14090bbd12f254e6ca81cfc868a8b6c522bc
Sn1ckas <99049886+Sn1ckas@users.noreply.github.com>
2022-03-15 21:57:22 +0100

    Create patchme.py

---
+++ web/patchme.py
@@ -0,0 +1,9 @@
+Beide Files downloaden
+im Python Script steht
+if( user_pw == "ak98" + \
+                   "-=90" + \
+                   "adfjhgj321" + \
+                   "sleuth9000"):
+
+  Passwort=ak98-=90adfjhgj321sleuth9000
+ 
