a1f63a67d7276b93a45678c700b93921c80b7566
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2022-03-20 23:07:41 +0100

    solve.py added

--- crypto/credstuff/solve.py
+++ crypto/credstuff/solve.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import codecs
+
 user="cultiris"
 counter=1
 
@@ -12,3 +14,17 @@ with open("leak/usernames.txt","r") as userfile:
         counter+=1
 
 with open("leak/passwords.txt","r") as passwdfile:
+    all_lines = passwdfile.readlines()
+    index = counter - 1
+    
+    print("Getting corresponding line with password")
+    
+    flag_enc = all_lines[index]
+
+    print("Password looks encoded (rot13)... " + flag_enc)
+    print("decoding...")
+
+    flag = codecs.decode(flag_enc, 'rot_13')
+
+    print("Got it!")
+    print(">>   " + flag)
